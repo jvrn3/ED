804e9afa5d1a70d54c8b20df612160de16dacc05
Joao <joaovitorrn388@gmail.com>
2018-11-12 14:43:09 -0200

    bug fixes

--- src/unit_test/test_sort.c
+++ src/unit_test/test_sort.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include "../modules/Sort/sort.h"
+#include "../modules/Carro/carro.h"
+
 int
 main(){
-	float a[] = {8,7,6,5,4,3,2,1};
-	
-	quick_sort(a, 0, 8);
+	Lista l = createList();
+	Carro c1 = newCarro("A", 10, 20, 15, 25);
+	Carro c2 = newCarro("B", 40, 50, 15, 25);
+	Carro c3 = newCarro("C", 20, 10, 15, 25);
+	insert(l, c1, 0);
+	insert(l, c2, 0);
+	insert(l, c3, 0);
 
-	for(int i = 0 ; i < 8; i++)
-		printf("%lf ", a[i]);
+	sort_list(l, car_cmp);
+	Node *n;
+	for(n = getFirst(l); n != NULL; n = getNext(n)){
+		printf("%lf\n", rect_get_y(carro_get_posic(list_get_data(n))));
+	}
 }
+
